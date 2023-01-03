15afb860df6d719b9ac97499942b496932eaf194
mibrix <ondrejga@gmail.com>
2023-01-03 19:48:58 +0100

    - presunutie funkcii updatovanie gamestatu do triedy GameState

--- SleepingQueens/dataStructures.py
+++ SleepingQueens/dataStructures.py
@@ -33,6 +33,46 @@ class GameState:
         self.awokenQueens = awokenQueens
         self.cardDiscardedLastTurn = cardDiscardedLastTurn
 
+    def updateGameStateCards(self, removeHandPos : list[HandPosition], addHandPos : list[Card]):
+
+        for y in range(len(removeHandPos)):
+            for i in self.cards.keys():
+                if (i.getCardIndex() == removeHandPos[y].getCardIndex() and
+                        i.getPlayerIndex() == removeHandPos[y].getPlayerIndex()):
+                    self.cards[i] = addHandPos[y]
+                    break
+
+    def updateGameStateAfterAttack(self, awQueenPos : AwokenQueenPosition, new_owner : int):
+        to_pop = AwokenQueenPosition(-1,-1)
+        temp_q = Queen(-1)
+        for i in self.awokenQueens.keys():
+            if (i.getCardIndex() == awQueenPos.getCardIndex() and
+                    i.getPlayerIndex() == awQueenPos.getPlayerIndex()):
+                to_pop = i
+                temp_q = self.awokenQueens[i]
+                break
+
+        #vymaz kralovnu z awokenQueens daneho hraca
+        if to_pop.cardIndex != -1 and temp_q.points != -1:
+            self.awokenQueens.pop(to_pop)
+            to_pop.playerIndex = new_owner
+
+            #ak hrac iba uspava kralovnu
+            if new_owner == -1:
+                q_idx = 0
+                if self.sleepingQueens != set():
+                    q_idx = max([i.cardIndex for i in self.sleepingQueens]) + 1
+                self.sleepingQueens.add(SleepingQueenPosition(q_idx))
+
+            #ak hrac utoci na kralovnu druheho hraca
+            else:
+                if self.awokenQueens == {}:
+                    to_pop.cardIndex = max([i.cardIndex for i in self.awokenQueens.keys()])+1
+                else:
+                    to_pop.cardIndex = 0
+
+                self.awokenQueens[to_pop] = temp_q
+
 class PlayerState:
     def __init__(self, cards : dict[int, Card], awokenQueens : dict[int,Queen]):
         self.cards = cards
