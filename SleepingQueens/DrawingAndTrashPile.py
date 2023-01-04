3af2519943d9dc7bd13887d0fb6fb2d00ddf515d
mibrix <ondrejga@gmail.com>
2023-01-04 11:33:56 +0100

    - vytvorenie strategy pattern pre DrawingAndTrashPile

--- SleepingQueens/DrawingAndTrashPile.py
+++ SleepingQueens/DrawingAndTrashPile.py
@@ -1,5 +1,65 @@
 from SleepingQueens.dataStructures import Card
 import random
+from abc import ABC, abstractmethod
+
+class DrawingStrategy(ABC):
+    @abstractmethod
+    def create_strategy(self):
+        pass
+
+class DiscardAll(DrawingStrategy):
+    def __init__(self, drawingPile : list[Card], trashPile : list[Card], discard : list[Card]):
+        self.drawingPile = drawingPile
+        self.trashPile = trashPile
+        self.discard = discard
+        self.output = []
+
+    def create_strategy(self) -> list[Card]:
+        number_to_draw = len(self.discard)
+        # discard
+        for i in range(len(self.discard)):
+            temp_vymaz = self.discard.pop(0)
+            self.trashPile.append(temp_vymaz)
+        #    self.discardtedThisTurn.append(temp_vymaz)
+
+        number_of_drawn = 0
+        while self.drawingPile != []:
+            self.output.append(self.drawingPile.pop(0))
+            number_of_drawn += 1
+
+        # shuffle
+        random.shuffle(self.trashPile)
+        self.drawingPile = self.drawingPile + self.trashPile
+        self.trashPile = []
+
+        for i in range(number_to_draw - number_of_drawn):
+            self.output.append(self.drawingPile.pop(0))
+
+        return self.output
+
+
+class ShuffleAndDiscard(DrawingStrategy):
+    def __init__(self, drawingPile: list[Card], trashPile: list[Card], discard: list[Card]):
+        self.drawingPile = drawingPile
+        self.trashPile = trashPile
+        self.discard = discard
+        self.output = []
+
+    def create_strategy(self) -> list[Card]:
+
+        # shuffle
+        random.shuffle(self.trashPile)
+        self.drawingPile = self.drawingPile + self.trashPile
+        self.trashPile = []
+
+        number_to_draw = len(self.discard)
+        # discard and redraw
+        for i in range(len(self.discard)):
+            temp_vymaz = self.discard.pop(0)
+            self.trashPile.append(temp_vymaz)
+            self.output.append(self.drawingPile.pop(0))
+
+        return self.output
 
 class DrawingAndTrashPile:
 
@@ -8,29 +68,11 @@ class DrawingAndTrashPile:
         self.trashPile = trashPile
         self.discardtedThisTurn : list[Card] = []
 
-    def discardAndDraw(self, discard : list[Card], discard_before_shuffle : int = 3) -> list[Card]:
-
-        output = []
+    def discardAndDraw(self, discard : list[Card], drawingStrategy : DrawingStrategy) -> list[Card]:
 
         #OCP solution for handling shuffle of cards
         if len(discard) > len(self.drawingPile):
-            # discard
-            for i in range(discard_before_shuffle):
-                temp_vymaz = discard.pop(0)
-                self.trashPile.append(temp_vymaz)
-                self.discardtedThisTurn.append(temp_vymaz)
-                output.append(self.drawingPile.pop(0))
-
-            # shuffle
-            random.shuffle(self.trashPile)
-            self.drawingPile = self.drawingPile + self.trashPile
-            self.trashPile = []
-
-            while discard != []:
-                temp_vymaz = discard.pop(0)
-                self.trashPile.append(temp_vymaz)
-                self.discardtedThisTurn.append(temp_vymaz)
-                output.append(self.drawingPile.pop(0))
+            output = drawingStrategy.create_strategy()
 
         else:
             #discard
