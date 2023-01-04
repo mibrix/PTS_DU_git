2996486100b7e365b6dd98d7f2127c70c8d473d0
mibrix <ondrejga@gmail.com>
2023-01-04 13:30:46 +0100

    - pridanie parametra messages

--- test/TestPlayer.py
+++ test/TestPlayer.py
@@ -5,16 +5,17 @@ from SleepingQueens.QueenCollection import SleepingQueens
 from SleepingQueens.Hand import Hand
 from SleepingQueens.DrawingAndTrashPile import DrawingAndTrashPile
 from SleepingQueens.dataStructures import GameState
+from SleepingQueens.Messages import Messages
 
 class TestPlayer(TestCase):
 
     def index(self, indexPl : int):
         a = Player(indexPl, PlayerState({},{}),SleepingQueens({},'sleep'), Hand(1,{},DrawingAndTrashPile([],[])),
-                   GameState(3,1,set(),{},{},[]))
+                   GameState(3,1,set(),{},{},[]), Messages())
         self.assertEqual(a.playerIdx, indexPl)
 
     def playerState(self):
         a = Player(0, PlayerState({},{}),SleepingQueens({},'sleep'), Hand(1,{},DrawingAndTrashPile([],[])),
-                   GameState(3,1,set(),{},{},[]))
+                   GameState(3,1,set(),{},{},[]), Messages())
         self.assertEqual(a.playerState.cards,{})
         self.assertEqual(a.playerState.awokenQueens, {})
